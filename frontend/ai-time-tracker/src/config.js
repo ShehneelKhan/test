843a098cb87d74aed747df5999954bd83b607dda
shehneel ahmed <shehneel.ahmed@squarera.com>
2025-08-29 15:26:18 +0100

    updated base url

--- frontend/ai-time-tracker/src/config.js
+++ frontend/ai-time-tracker/src/config.js
@@ -1,3 +1,4 @@
 // src/config.js
-export const BASE_URL = "https://smlrn389-8000.inc1.devtunnels.ms";
+// export const BASE_URL = "https://smlrn389-8000.inc1.devtunnels.ms";
 // export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://test-8d3m.onrender.com";
